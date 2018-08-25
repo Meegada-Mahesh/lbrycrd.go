e988a72bccd22d751ace7c02ac29e113b1de026e
Tzu-Jung Lee <roylee@lbry.io>
2018-08-25 14:53:21 -0700

    txscript: update for LBRY Blockchain
    - Update MaxScriptElementSize from 520 t0 20,000 bytes
    - Update MaxScriptSize from 10,000 t0 20,005 bytes
    - Implement LBRY claim script
    - Recognize LBRY claim script OPCODES
    - Update testcases

--- txscript/example_test.go
+++ txscript/example_test.go
@@ -24,7 +24,7 @@ func ExamplePayToAddrScript() {
 	// which is useful to ensure the accuracy of the address and determine
 	// the address type.  It is also required for the upcoming call to
 	// PayToAddrScript.
-	addressStr := "12gpXQVcCL2qhTNQgyLVdCFG2Qs2px98nV"
+	addressStr := "bER6Ddq6YfRKDJDmmdeaqrP8XHmDJcYSJQ"
 	address, err := btcutil.DecodeAddress(addressStr, &chaincfg.MainNetParams)
 	if err != nil {
 		fmt.Println(err)
@@ -75,7 +75,7 @@ func ExampleExtractPkScriptAddrs() {
 
 	// Output:
 	// Script Class: pubkeyhash
-	// Addresses: [12gpXQVcCL2qhTNQgyLVdCFG2Qs2px98nV]
+	// Addresses: [bER6Ddq6YfRKDJDmmdeaqrP8XHmDJcYSJQ]
 	// Required Signatures: 1
 }
 
